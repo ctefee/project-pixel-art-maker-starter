0af44223ab1b2aacf89a1fa14d968cdfb5bc06df
ctefee <steph.sklyar@gmail.com>
2018-01-18 18:22:06 -0500

    saving

--- designs.js
+++ designs.js
@@ -1,33 +1,39 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+	let input_height = $('#input_height').val(); // get access to height
+	var input_width = $('#input_width').val();   // get access to width
+	var tbl = $('#pixel_canvas'); // get access to html table
 
-// Your code goes here!
+	$(tbl).html(''); // erase the table content
 
-let input_height = $('#input_height').val(); //  get access to height
-let input_width = $('#input_width').val();   //  get access to width
-let tbl = $('#pixel_canvas'); // get access to html table
+	var tableHtml = ''; // create variable to new table content
 
-$(tbl).html(''); // erase the table content
+	// loop through each table row
+	for (var i = 0; i < input_height; i++) {
+		tableHtml = tableHtml + '<tr>';
+		// loop through each column
+		for (var k = 0; k < input_width; k++) {
+			tableHtml = tableHtml + '<td onclick="  $(this).css(  \'background-color\', $(\'#colorPicker\').val()  )  "></td>';
+		}
+		tableHtml = tableHtml + '</tr>';
+		console.log(tableHtml);
 
-let tableHtml = ''; // create variable to new table content
+	}
 
-// loop through each table row
-for (let i = 0; i < input_height; i++) {
-  tableHtml = tableHtml + '<tr>';
-
-  // loop through each column
-  for (let k = 0; k < input_width; k++) {
-    tableHtml = tableHtml + '<td onclick="  $(this).css(  \'background-color\', $(\'#colorPicker\').val()  )  "></td>';
-  }
-  tableHtml = tableHtml + '</tr>';
-
-  console.log(tableHtml);
-
-  }
-
-  $(tbl).html(tableHtml); // put new content into the table
+	$(tbl).html(tableHtml); // put new content into the table
 }
+
+
+/*
+<table>
+
+	<tr> // row
+		<td  onclick=" $(this).css('background-color', $('#colorPicker').val()  )"></td> // column
+		<td  onclick=" $(this).css('background-color', $('#colorPicker').val()  )"></td> // column
+	</tr>
+
+</table>
+*/
